b4c9ceaa4ca1d35db47940ba2ab2c010efd7b1d8
ansh batra <marsab@cpan.org>
2011-07-29 12:02:00 +0000

    Import of MARSAB/Odb-New from CPAN.
    gitpan-cpan-distribution: Odb-New
    gitpan-cpan-version:      
    gitpan-cpan-path:         MARSAB/Odb-New.tar.gz
    gitpan-cpan-author:       MARSAB
    gitpan-cpan-maturity:     released

---
+++ lib/Odb/New.pm
@@ -0,0 +1,88 @@
+package Odb::New;
+
+use 5.010001;
+use strict;
+use warnings;
+
+require Exporter;
+our $VERSION = '0.01';
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Odb::New - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Odb::New;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Odb::New, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head1 CONTENTS
+Test::Harness
+Digest::SHA
+Compress::Raw::Zlib
+Term::ReadKey
+Compress::Raw::Bzip2
+HTML::Tagset
+Pod::Escapes
+Net::IP
+Sys::Hostname::Long
+Storable
+IO::Stringy
+OLE::Storage_Lite
+Scalar::Util::Numeric
+Net::Ping
+Net::CIDR::Lite
+Crypt::OpenSSL::Random
+Time::Piece
+Class::Singleton
+Data::Dump
+FCGI
+GD
+BerkeleyDB
+DB_File
+Class::Data::Inheritable
+HTML::HTMLDoc
+File::Grep
+mod_perl2
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+ansh, E<lt>ansh@(none)E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by ansh
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
